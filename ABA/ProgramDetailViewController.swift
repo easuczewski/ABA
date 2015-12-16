4bf0fa0b14e99e9111ad203a51e374822d13cabb
Edward Suczewski <ed.suczewski@gmail.com>
2015-12-16 16:12:58 -0700

    Implement ProgramDetailVC / will add header button
    Signed-off-by: Edward Suczewski <ed.suczewski@gmail.com>

--- ABA/ProgramDetailViewController.swift
+++ ABA/ProgramDetailViewController.swift
@@ -7,8 +7,14 @@
 //
 
 import UIKit
+@IBDesignable
 
-class ProgramDetailViewController: UIViewController {
+class ProgramDetailViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var selectedStudent: Student?
+    var selectedProgram: Program?
+    var shortTermObjectivesForSelectedProgram: [ProgramShortTermObjective] = []
+    var tacticsForSelectedProgram: [ProgramTactic] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,7 +27,76 @@ class ProgramDetailViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    
+    // MARK: UITableViewDataSource
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 6
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section <= 3 {
+            return 1
+        } else if section == 4 {
+            return shortTermObjectivesForSelectedProgram.count
+        } else if section == 5 {
+            return tacticsForSelectedProgram.count
+        } else {
+            return 0
+        }
+    }
+    
+    func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch section {
+        case 0:
+            if let student = selectedStudent {
+                return "Program for \(student.name)"
+            } else {
+                return "Program"
+            }
+        case 1:
+            return "Domain"
+        case 2:
+            return "Antecedent"
+        case 3:
+            return "LTO - swipe to complete"
+        case 4:
+            return "STO - swipe to complete"
+        case 5:
+            return "Tactic - swipe to start/stop"
+        default:
+            return ""
+        }
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("programDetailCell", forIndexPath: indexPath)
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerFrame: CGRect = tableView.frame
+        let headerView: UIView = UIView(frame: CGRectMake(0, 0, headerFrame.size.width, headerFrame.size.height))
+        headerView.backgroundColor = UIColor.purpleColor()
 
+        if section == 5 {
+            let button: UIButton = UIButton(type: .ContactAdd) as UIButton
+//            let image = UIImage(named: "Plus")
+//            button.setBackgroundImage(image, forState: .Normal)
+            headerView.addSubview(button)
+            let margins = headerView.layoutMarginsGuide
+            let constraint = button.trailingAnchor.constraintEqualToAnchor(margins.trailingAnchor)
+            constraint.active = true
+//            button.heightAnchor.constraintEqualToAnchor(margins.heightAnchor).active = true
+//            button.widthAnchor.constraintEqualToAnchor(button.heightAnchor).active = true
+            button.addConstraint(constraint)
+            return headerView
+
+        } else {
+            return headerView
+        }
+        
+    }
     /*
     // MARK: - Navigation
 
